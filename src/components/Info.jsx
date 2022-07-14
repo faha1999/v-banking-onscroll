65127ebe0d8a17fe42d2e4388fd0f464bbc19699
Kawsar Ahmed Fahad <kafaha1999@gmail.com>
2022-07-14 14:57:18 +0600

    innitial info added

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,25 +6,57 @@ const InfoContainer = styled.div`
   background: ${({ lightBg }) => (lightBg ? '#f9f9f9' : '#010606')};
 `;
 
+const Row = styled.div`
+  grid-template-columns: ${({ imgStart }) =>
+    imgStart ? `'col2 col1'` : `'col1 col2'`};
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: ${({ imgStart }) =>
+      imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`};
+  }
+`;
+
+const Heading = styled.h1`
+  color: ${({ lightText }) => (lightText ? '#f7f8fa' : '#010606')};
+`;
+
+const SubTitle = styled.p`
+  color: ${({ darkText }) => (darkText ? '#010606' : '#fff')};
+`;
+
+const Column1 = styled.div`
+  grid-area: cal1;
+`;
+
+const Column2 = styled.div`
+  grid-area: cal2;
+`;
+
 export const Info = () => {
   return (
     <>
       <InfoContainer className="infoContainer">
         <div className="wrapper">
-          <div className="row">
-            <div className="column1">
+          <Row className="row">
+            <Column1 className="column1">
               <div className="text">
-                <div className="topLine">TopLine</div>
-                <div className="heading">Heading</div>
-                <div className="subtitle">subtitle</div>
+                <p className="topLine">TopLine</p>
+                <Heading className="heading">Heading</Heading>
+                <SubTitle className="subtitle">subtitle</SubTitle>
                 <div className="btn">
                   <Button className="button" to="home">
                     Button
                   </Button>
                 </div>
               </div>
-            </div>
-          </div>
+            </Column1>
+
+            <Column2 className="column2">
+              <div className="imgWrap">
+                <img src="" alt="" />
+              </div>
+            </Column2>
+          </Row>
         </div>
       </InfoContainer>
     </>
