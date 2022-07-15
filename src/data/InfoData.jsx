b10c01ca5e4defbacd7c7291df170dabb8de6361
Kawsar Ahmed Fahad <kafaha1999@gmail.com>
2022-07-15 23:40:14 +0600

    added data

--- src/data/InfoData.jsx
+++ src/data/InfoData.jsx
@@ -1,1 +1,17 @@
-export const InfoData = {};
+export const InfoData = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Bank',
+  Headline: 'Ultimate Transaction with zero fees',
+  description:
+    'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, alias.',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../assets/video/faha1999'),
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
